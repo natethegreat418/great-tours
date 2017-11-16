9bd5a52e382045e84b2297d17ef4a534537eff53
Nathan Marcus <nathanimarcus@gmail.com>
2017-11-15 21:03:00 -0500

    Added base table migrations, debug route

---
+++ database/migrations/2017_11_16_014122_create_tours_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateToursTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tours', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('code');
+            $table->string('tile_image');
+            $table->longText('descr');
+            $table->string('continent');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tours');
+    }
+}

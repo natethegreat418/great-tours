4b042a4a3b7fb2d0a18024989ff95e90eb46a0f7
Nathan Marcus <nathanimarcus@gmail.com>
2017-11-15 20:35:01 -0500

    Added readme, env check route

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,10 @@
 */
 
 Route::view('/', 'welcome');
+
+Route::GET('/env', function () {
+    dump(config('app.name'));
+    dump(config('app.env'));
+    dump(config('app.debug'));
+    dump(config('app.url'));
+});

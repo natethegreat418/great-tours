9ec4d7b2e5b6c82f92338029ba5a7cb0d65a7770
Nathan Marcus <nathanimarcus@gmail.com>
2017-11-20 18:23:48 -0500

    test prod commit from new machine

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,6 @@
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
     </head>
     <body>
-      <h1>Welcome to GreatTours</h1>
+      <h1>Welcome to GreatTours!</h1>
     </body>
 </html>
